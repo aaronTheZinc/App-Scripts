e4be4f94fba1628e6195e023a82c360aa467347a
Aaron Marsh <aaronmarsh755@gmail.com>
2021-05-06 23:41:10 -0400

    init

---
+++ deleteAuthUsers.js
@@ -0,0 +1,6 @@
+// Open Console And Paste To Rid Test Users.
+setInterval(() => {
+    document.getElementsByClassName('edit-account-button mat-focus-indicator mat-menu-trigger mat-icon-button mat-button-base')[0].click()
+    document.getElementsByClassName('mat-focus-indicator mat-menu-item ng-star-inserted')[2].click()
+    document.getElementsByClassName('confirm-button mat-focus-indicator mat-raised-button mat-button-base mat-warn')[0].click()
+}, 1000)
